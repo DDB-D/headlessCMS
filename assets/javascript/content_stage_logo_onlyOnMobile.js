ec1869102c29d7ed53ebe1f391ead62bd9455e09
hartgeld <lorehartgeld@gmail.com>
2019-08-21 18:32:32 +0200

    mobile content:stage set up

---
+++ assets/javascript/content_stage_logo_onlyOnMobile.js
@@ -0,0 +1,7 @@
+function content_stage_logoType_transformType(){
+  new CircleType(document.getElementById('stage_logoType'));
+}
+
+$( document ).ready(function() {
+    content_stage_logoType_transformType();
+});
