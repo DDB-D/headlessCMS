791510b52059c8852720fe8235690fa6e3a19633
hartgeld <lorehartgeld@gmail.com>
2019-10-11 13:34:22 +0200

    added dragable function, added springs between all nodes

--- assets/javascript/p5-sketch.js
+++ assets/javascript/p5-sketch.js
@@ -1,17 +1,82 @@
+// get target div
 var containerNode = document.getElementById("content_stage_bg-img");
-
+// get width and height of target div
 var containerWidth = containerNode.offsetWidth;
 var containerHeight = containerNode.offsetHeight;
-
+// load toxilib components
 var VerletPhysics2D = toxi.physics2d.VerletPhysics2D,
     VerletParticle2D = toxi.physics2d.VerletParticle2D,
     VerletSpring2D = toxi.physics2d.VerletSpring2D,
     VerletMinDistanceSpring2D = toxi.physics2d.VerletMinDistanceSpring2D,
     Vec2D = toxi.geom.Vec2D,
-    Rect = toxi.geom.Rect;
+    Rect = toxi.geom.Rect,
+    AttractionBehavior = toxi.physics2d.behaviors.AttractionBehavior,
+    GravityBehavior = toxi.physics2d.behaviors.GravityBehavior;
 
-
+// start: p5 sketch
 var sketch = function( p ) {
+
+  var options = {
+    numClusters: 50,
+    particleRadius: 10,
+    showPhysics: true,
+    showParticles: true,
+    springStrength: 0.01,
+    minDistanceSpringStrength: 0.05
+  };
+
+/*
+  var gui = new dat.gui.GUI();
+  gui.add(options, 'numClusters', 5, 16).step(1);
+  gui.add(options, 'showPhysics');
+  gui.add(options, 'showParticles');
+  gui.add(options, 'springStrength', 0, 0.1).step(0.001);
+  gui.add(options, 'minDistanceSpringStrength', 0, 0.1).step(0.001);
+  gui.add({ makeGraph: p.makeGraph }, 'p.makeGraph').name('New Graph');
+*/
+
+  var clusters,
+      physics,
+      mouseAttractor
+      selected;
+
+  //make particles draggable
+  //var dragging = false; // Is the object being dragged?
+  //var rollover = false; // Is the mouse over the ellipse?
+
+  var snapDist=10*10;
+  var selected = new VerletParticle2D(null);
+  var mousePos;
+
+  p.setup = function() {
+
+    myp5GlobalSketchVar = p.createCanvas(containerWidth, containerHeight);
+
+    physics = new VerletPhysics2D();
+    physics.setWorldBounds(new Rect(10, 10, p.width-20, p.height-20));
+
+    //mouseAttractor = new AttractionBehavior();
+
+    p.makeGraph();
+  }
+
+  p.draw = function(){
+    physics.update();
+    p.background(255);
+    if(options.showParticles){
+        clusters.forEach(function(c){
+            c.display();
+        });
+    }
+    if(options.showPhysics){
+        p.forEachNested(clusters, function(ci, cj){
+                      ci.showConnections();
+                      ci.showConnections(cj);
+        });
+    }
+
+
+  }
 
   p.times = function(n, fn){
       var arr = [];
@@ -32,56 +97,14 @@ var sketch = function( p ) {
     }
   }
 
-  var options = {
-    numClusters: 15,
-    particleRadius: 10,
-    showPhysics: true,
-    showParticles: true,
-    springStrength: 0.01,
-    minDistanceSpringStrength: 0.05
-  };
-/*
-  var gui = new dat.gui.GUI();
-  gui.add(options, 'numClusters', 5, 16).step(1);
-  gui.add(options, 'showPhysics');
-  gui.add(options, 'showParticles');
-  gui.add(options, 'springStrength', 0, 0.1).step(0.001);
-  gui.add(options, 'minDistanceSpringStrength', 0, 0.1).step(0.001);
-  gui.add({ makeGraph: p.makeGraph }, 'p.makeGraph').name('New Graph');
-*/
-
-  var clusters,
-      physics,
-      selected;
-
-  var bottomPadding = 200;
-
-
-  //make particles draggable
-
-  var dragging = false; // Is the object being dragged?
-  var rollover = false; // Is the mouse over the ellipse?
-
-
-
-  p.setup = function() {
-
-    myp5GlobalSketchVar = p.createCanvas(containerWidth, containerHeight);
-
-    physics = new VerletPhysics2D();
-    physics.setWorldBounds(new Rect(10, 10, p.width-20, p.height-20));
-
-    p.makeGraph();
-  }
-
   p.makeGraph = function(){
 
     physics.clear();
 
     clusters = p.times(options.numClusters, function(){
         return new p.Cluster(
-            Math.floor(p.random(1,5)),
-            Math.floor(p.random(30, 80)),
+            1,
+            Math.floor(p.random(15, 45)),
             new Vec2D((p.width/4)*3, p.height/2)
         );
     });
@@ -91,24 +114,7 @@ var sketch = function( p ) {
     })
   }
 
-  p.draw = function(){
-    physics.update();
-    p.background(255);
 
-    if(options.showParticles){
-        clusters.forEach(function(c){
-            c.display();
-        });
-    }
-    if(options.showPhysics){
-        p.forEachNested(clusters, function(ci, cj){
-                      ci.showConnections();
-                      ci.showConnections(cj);
-        });
-    }
-
-    //draggable particles check
-  }
 
   p.Cluster = function(n, d, center){
     this.diameter = d;
@@ -136,12 +142,22 @@ var sketch = function( p ) {
     this.nodes.forEach(function(pi){
         other.nodes.forEach(function(pj){
             physics.addSpring(
+                new VerletSpring2D(
+                    pi,
+                    pj,
+                    (selfDiam + other.diameter)*5.5,
+                    0.0001
+                )
+
+            );
+            physics.addSpring(
                 new VerletMinDistanceSpring2D(
                     pi,
                     pj,
-                    (selfDiam + other.diameter) * 1.15,
-                    options.minDistanceSpringStrength
+                    (selfDiam + other.diameter)*5,
+                    0.9
                 )
+
             );
         })
     });
@@ -175,14 +191,38 @@ var sketch = function( p ) {
   };
 
   p.mousePressed = function(){
-    /*p.forEachNested(this.nodes, function(pi, pj){
-      console.log("a node");
-    });*/
+
+    mousePos = new Vec2D(p.mouseX, p.mouseY);
+
     clusters.forEach(function(c){
-      console.log("a node");
-    });  }
+      c.nodes.forEach(function(n){
+        if (n.distanceToSquared(mousePos)<snapDist) {
+          selected=n;
+          selected.lock();
+          //p.break;
+          return false;
+        }
+      });
+    });
 
+    mouseAttractor = new AttractionBehavior(mousePos, 250, 0.9);
+    physics.addBehavior(mouseAttractor);
+  }
 
+  p.mouseDragged = function(){
+    if (selected!=null) {
+      selected.set(p.mouseX, p.mouseY);
+    }
+    mousePos.set(p.mouseX, p.mouseY);
+  }
+
+  p.mouseReleased = function() {
+    if (selected!=null) {
+      selected.unlock();
+      selected=null;
+    }
+    physics.removeBehavior(mouseAttractor);
+  }
 }
 
 myp5 = new p5(sketch, containerNode);
