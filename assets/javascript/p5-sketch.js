94c4303479254a907205765a5d2a1df5f1da34c8
hartgeld <lorehartgeld@gmail.com>
2019-10-14 12:12:03 +0200

    added POI, locked particle on click

--- assets/javascript/p5-sketch.js
+++ assets/javascript/p5-sketch.js
@@ -21,8 +21,8 @@ var sketch = function( p ) {
     particleRadius: 8,
     showPhysics: true,
     showParticles: true,
-    springStrength: 0.001,
-    minDistanceSpringStrength: 0.00015
+    springStrength: 0.0005,
+    minDistanceSpringStrength: 0.0001
   };
 
 /*
@@ -47,6 +47,13 @@ var sketch = function( p ) {
   var snapDist=10*10;
   var selected = new VerletParticle2D(null);
   var mousePos;
+  var node_poi;
+
+  var myFont;
+
+  p.preload = function() {
+    myFont = p.loadFont('/assets/fonts/Inter-Medium.woff');
+  }
 
   p.setup = function() {
 
@@ -58,10 +65,20 @@ var sketch = function( p ) {
     //physics.addBehavior(new GravityBehavior(new Vec2D(0, 0.15)));
 
     mousePos = new Vec2D(p.mouseX, p.mouseY);
-    mouseAttractor = new AttractionBehavior(mousePos, 75, 2.5);
+    mouseAttractor = new AttractionBehavior(mousePos, 75, 0.5);
     physics.addBehavior(mouseAttractor);
 
     p.makeGraph();
+
+    for(var i=1; i<clusters.length; i++){
+      //console.log("node");
+      var myCluster = clusters[0];
+
+      node_poi = myCluster[0];
+
+      //console.log("myCluster-pos: " + node_poi.x);
+
+    }
   }
 
   p.draw = function(){
@@ -71,6 +88,12 @@ var sketch = function( p ) {
         clusters.forEach(function(c){
             c.display();
         });
+        /*
+        clusters.forEach(function(c){
+          c.displayPOI();
+        });
+        */
+        clusters[0].displayPOI();
     }
     if(options.showPhysics){
         p.forEachNested(clusters, function(ci, cj){
@@ -79,6 +102,9 @@ var sketch = function( p ) {
         });
     }
     mousePos.set(p.mouseX, p.mouseY);
+
+
+    //POI/Tooltip
 
   }
 
@@ -107,7 +133,8 @@ var sketch = function( p ) {
 
     clusters = p.times(options.numClusters, function(){
         return new p.Cluster(
-            1,
+          1,
+
             Math.floor(p.random(15, 45)),
             new Vec2D((p.width/4)*3, p.height/2)
         );
@@ -145,6 +172,14 @@ var sketch = function( p ) {
     });
   };
 
+  p.Cluster.prototype.displayPOI = function(){
+
+    this.nodes.forEach(function(n){
+        n.displayPOI();
+    });
+
+  };
+
   p.Cluster.prototype.connect = function(other){
     var selfDiam = this.diameter;
     this.nodes.forEach(function(pi){
@@ -154,7 +189,7 @@ var sketch = function( p ) {
                     pi,
                     pj,
                     (selfDiam + other.diameter)*3.5,
-                    0.001
+                    options.springStrength
                 )
 
             );
@@ -163,7 +198,7 @@ var sketch = function( p ) {
                     pi,
                     pj,
                     (selfDiam + other.diameter)*5,
-                    0.2
+                    options.minDistanceSpringStrength
                 )
 
             );
@@ -189,20 +224,40 @@ var sketch = function( p ) {
 
   p.Node = function(pos){
     VerletParticle2D.call(this, pos);
-    physics.addBehavior(new AttractionBehavior(this, 200, -0.2));
-    physics.addParticle(this.lock());
+    physics.addBehavior(new AttractionBehavior(this, 200, -0.02));
+    physics.addParticle(this.unlock());
   }
 
   p.Node.prototype = Object.create(VerletParticle2D.prototype);
 
   p.Node.prototype.display = function(){
-
     p.fill(0);
     p.ellipse(this.x, this.y, options.particleRadius, options.particleRadius);
+  };
 
+  p.Node.prototype.displayPOI = function(){
+    p.fill(255,0,0);
+    p.ellipse(this.x, this.y, options.particleRadius, options.particleRadius);
+    p.noFill();
+    p.stroke(255,0,0);
+    p.ellipse(this.x, this.y, options.particleRadius*1.5, options.particleRadius*1.5);
 
+    if (this.distanceToSquared(mousePos)<snapDist) {
+      p.textFont(myFont);
+      p.textSize(16);
+      p.fill(255,0,0);
+      p.noStroke();
+      p.text("Zielgruppe", this.x + 10, this.y + 3);
+      //selected=n;
+      //selected.lock();
+      //p.break;
+      //return false;
+
+      console.log("rollo!!!");
+    }
 
   };
+
 
   p.mousePressed = function(){
 
@@ -234,8 +289,8 @@ var sketch = function( p ) {
 
   p.mouseReleased = function() {
     if (selected!=null) {
-      selected.unlock();
-      selected=null;
+      //selected.unlock();
+      //selected=null;
     }
     //physics.removeBehavior(mouseAttractor);
   }
