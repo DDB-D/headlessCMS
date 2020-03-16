ff37ee626757097882c35474583186769dd15f0d
hartgeld <mail@holgerardelt.de>
2020-03-16 21:02:50 +0100

    added simple preloader. to be optimized

---
+++ assets/javascript/ui_preloader.js
@@ -0,0 +1,19 @@
+/*
+var preloader_logo = document.getElementById("myPreloader");
+console.log(preloader_logo);
+preloader_logo.setAttribute("fill" "red");
+*/
+
+const fillColor_css = getComputedStyle(document.documentElement).getPropertyValue("--color-type");
+
+var doc = document.getElementById('preloader-fade');
+console.log(doc);
+var p = doc.querySelectorAll("path");
+
+p.forEach(function(element, index){
+  element.setAttribute("fill", fillColor_css);
+})
+
+$(window).on('load', function(){
+  $("#preloader-fade").fadeOut("slow");
+});
